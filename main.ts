5eee2108ab5527277090915d916e59b8016ca451
MITCHELLE55 <atmich55@gmail.com>
2026-04-01 11:47:09 +0300

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (hopes_and_pause.vy == 0) {
+        hopes_and_pause.vy = -250
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
+    game.gameOver(true)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.melt)
+})
+let hopes_and_pause: Sprite = null
 scene.setBackgroundColor(9)
-let mySprite = sprites.create(img`
+hopes_and_pause = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -17,4 +30,7 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
+controller.moveSprite(hopes_and_pause, 100, 0)
+tiles.setCurrentTilemap(tilemap`level1`)
+hopes_and_pause.ay = 350
+scene.cameraFollowSprite(hopes_and_pause)
